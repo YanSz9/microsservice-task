855e0d36f3a30f6fcd75879cc79488385fe1fec7
YanSz9 <yansouza009@gmail.com>
2024-05-07 00:57:07 -0300

    feat: using only the body message

--- src/main/java/com/yancorrea/publisherapi/consumer/PaymentRequestConsumer.java
+++ src/main/java/com/yancorrea/publisherapi/consumer/PaymentRequestConsumer.java
@@ -3,10 +3,12 @@ package com.yancorrea.publisherapi.consumer;
 import java.util.Random;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
+import org.springframework.amqp.support.converter.SimpleMessageConverter;
 
 import com.yancorrea.publisherapi.producer.PaymentErrorProducer;
 import com.yancorrea.publisherapi.producer.PaymentSuccessProducer;
@@ -19,8 +21,9 @@ public class PaymentRequestConsumer {
     private PaymentSuccessProducer sucessProducer;
 
     @RabbitListener(queues = { "payment-request-queue" })
-    public void receiveMessage(@Payload Message message) {
-        System.out.println(message);
+    public void receiveMessage(Message<String> message) {
+        String messageBody = message.getPayload();
+        System.out.println(messageBody);
         if (new Random().nextBoolean()) {
             sucessProducer.generateResponse("Mensagem de sucesso pagamento" + message);
         } else
