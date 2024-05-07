b51574400ca8bcb7a580ce24a304fde60ee67daf
YanSz9 <yansouza009@gmail.com>
2024-05-07 00:32:13 -0300

    feat: final implementation

--- src/main/java/com/yancorrea/publisherapi/consumer/PaymentRequestConsumer.java
+++ src/main/java/com/yancorrea/publisherapi/consumer/PaymentRequestConsumer.java
@@ -1,15 +1,29 @@
 package com.yancorrea.publisherapi.consumer;
 
+import java.util.Random;
+
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+import com.yancorrea.publisherapi.producer.PaymentErrorProducer;
+import com.yancorrea.publisherapi.producer.PaymentSuccessProducer;
+
 @Component
 public class PaymentRequestConsumer {
+    @Autowired
+    private PaymentErrorProducer errorProducer;
+    @Autowired
+    private PaymentSuccessProducer sucessProducer;
+
     @RabbitListener(queues = { "payment-request-queue" })
     public void receiveMessage(@Payload Message message) {
         System.out.println(message);
-
+        if (new Random().nextBoolean()) {
+            sucessProducer.generateResponse("Mensagem de sucesso pagamento" + message);
+        } else
+            errorProducer.generateResponse("Erro no pagamento" + message);
     }
 }
